db48748dc6ae433d99f43eb97d92bdfd70f01a50
Talha <talhamaqsood890@gmail.com>
2024-02-04 14:42:58 +0300

    Styling

---
+++ src/styles/buttons.js
@@ -0,0 +1,62 @@
+import {Platform} from 'react-native';
+import * as Colors from './colors';
+import * as Outlines from './outlines';
+import * as Sizing from './sizing';
+import * as Typography from './typography';
+
+const bar = {
+  primary: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: Sizing.layout.x30,
+    borderRadius: Outlines.borderRadius.base,
+    backgroundColor: Colors.primary.brand,
+  },
+  secondary: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    padding: Sizing.layout.x10,
+    borderRadius: Outlines.borderRadius.base,
+  },
+};
+
+const barText = {
+  primary: {
+    ...Typography.fontSize.x30,
+    ...Typography.fontWeight.semibold,
+    color: Colors.neutral.white,
+  },
+  secondary: {
+    ...Typography.fontSize.x10,
+    ...Typography.fontWeight.regular,
+    color: Colors.neutral.s500,
+  },
+};
+
+const circular = {
+  primary: {
+    height: Sizing.layout.x30,
+    width: Sizing.layout.x30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary.brand,
+    borderRadius: Outlines.borderRadius.max,
+  },
+};
+
+const opacity = state => {
+  const opacity = state.pressed ? 0.65 : 1;
+  return {opacity};
+};
+
+const applyOpacity = style => {
+  return state => {
+    return {
+      ...style,
+      ...opacity(state),
+    };
+  };
+};
+
+export {bar, barText, circular, applyOpacity};
---
+++ src/styles/sizing.js
@@ -0,0 +1,68 @@
+import { Dimensions } from "react-native";
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get("screen");
+
+const screen = {
+  width: screenWidth,
+  height: screenHeight,
+};
+
+const layout = {
+  x1: 1,
+  x2: 2,
+  x3: 3,
+  x5: 5,
+  x7: 7,
+  x10: 10,
+  x15: 14,
+  x20: 18,
+  x30: 26,
+  x40: 34,
+  x50: 42,
+  x60: 50,
+  x70: 64,
+  x80: 86,
+  x90: 120,
+  x100: 130,
+  x110: 150,
+  x120: 170,
+  x130: 200,
+  x140: 230,
+};
+
+const x1 = layout.x1;
+const x2 = layout.x2;
+const x3 = layout.x3;
+const x5 = layout.x5;
+const x7 = layout.x7;
+const x10 = layout.x10;
+const x15 = layout.x15;
+const x20 = layout.x20;
+const x30 = layout.x30;
+const x40 = layout.x40;
+const x50 = layout.x50;
+const x60 = layout.x60;
+const x70 = layout.x70;
+const x80 = layout.x80;
+const x90 = layout.x90;
+const x100 = layout.x100;
+const x110 = layout.x110;
+const x120 = layout.x120;
+const x130 = layout.x130;
+const x140 = layout.x140;
+
+const icons = {
+  x10: 14,
+  x15: 17,
+  x20: 20,
+  x25: 25,
+  x30: 30,
+  x40: 40,
+};
+
+const iconStroke = {
+  x1: 1,
+  x2: 2,
+};
+
+export { screen, layout, x1, x2, x3, x5, x7, x10, x15, x20, x30, x40, x50, x60, x70, x80, x90, x100, x110, x120, x130, x140, icons, iconStroke };
